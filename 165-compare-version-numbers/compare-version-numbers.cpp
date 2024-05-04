77a9b72ad2fde5e4fd6922e63867e4ce71f773fa
Rishu Kumar <106136366+rishu0007@users.noreply.github.com>
2024-05-04 22:50:54 +0530

    Time: 3 ms (39.13%) | Memory: 7.4 MB (77.15%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.cpp
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -6,20 +6,22 @@ public:
         int num1 = 0, num2 = 0;
         while(i < n1 || j < n2) {
             while(i < n1 && version1[i] != '.') {
-                num1 = num1*10+(version1[i]-'0');
+                num1 = num1 * 10 + (version1[i] - '0');
                 i++;
             }
             while(j < n2 && version2[j] != '.') {
-                num2 = num2*10+(version2[j] - '0');
+                num2 = num2 * 10 + (version2[j] - '0');
                 j++;
             }
-            if(num1 > num2) 
-                return 1;
-            if(num1 < num2) 
+            if(num1 < num2) {
                 return -1;
+            }
+            if(num1 > num2) {
+                return 1;
+            }
             i++;
             j++;
-            num1 = 0; 
+            num1 = 0;
             num2 = 0;
         }
         return 0;
